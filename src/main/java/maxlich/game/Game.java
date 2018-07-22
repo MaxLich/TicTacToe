9241b2139150436e701a5d27a8ffda96720df015
MaxLich <lich-max@mail.ru>
2018-07-22 22:50:34 +0300

    edit build-script

--- src/main/java/maxlich/game/Game.java
+++ src/main/java/maxlich/game/Game.java
@@ -1,25 +1,10 @@
 package maxlich.game;
 
-import maxlich.game.controller.Controller;
-import maxlich.game.model.Model;
-import maxlich.game.view.View;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Game {
-    private View view;
-    private Controller controller;
-    private Model model;
-
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
-        //Game game = context.getBean(Game.class);
     }
-
-
-  /*  public Game(View view, Controller controller, Model model) {
-        this.view = view;
-        this.controller = controller;
-        this.model = model;
-    }*/
 }
